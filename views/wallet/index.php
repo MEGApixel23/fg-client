e5f3e77dffcc24a4e948293c4ed90214138e93d2
MEGApixel23 <megapixel23@ya.ru>
2016-02-09 14:51:01 +0200

    Add wallet button was added

--- views/wallet/index.php
+++ views/wallet/index.php
@@ -5,6 +5,7 @@
  */
 
 use app\assets\MobileAppAsset;
+use yii\helpers\Url;
 
 $this->title = 'Wallets';
 $this->registerJsFile('/js/app/wallet/get.js', ['depends' => MobileAppAsset::className()]);
@@ -22,14 +23,7 @@ $this->registerJsFile('/js/app/wallet/get.js', ['depends' => MobileAppAsset::cla
     </tbody>
 </table>
 
-<!--<div class="fixed-action-btn horizontal" style="bottom: 45px; right: 24px;">-->
-<!--    <a class="btn-floating btn-large red">-->
-<!--        <i class="large material-icons">mode_edit</i>-->
-<!--    </a>-->
-<!--    <ul>-->
-<!--        <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>-->
-<!--        <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>-->
-<!--        <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>-->
-<!--        <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>-->
-<!--    </ul>-->
-<!--</div>-->
+<div class="controls">
+    <a href="<?= Url::to(['/wallet/create']) ?>"
+       class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+</div>
